2ce049e8f2dbce4440e2ca6d5c9d792ff86103aa
Daniel Tang <tangjuqian@yahoo.com.sg>
2020-06-05 18:59:24 +0800

    up to before gravity ex

---
+++ Assets/Scenes/MouseCameraControls.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCameraControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    float LookInputX()
+    {
+        return Input.GetAxisRaw("Mouse X")*sens;
+    }
+    float LookInputY()
+    {
+        return Input.GetAxisRaw("Mouse Y")*sens;
+    }
+    
+    public Camera playerCamera; //for vertical we are adjusting the camera angle with player fixed, and vice versa for horizontal
+    private float camVertAngle = 0;
+    public float sens=100; //increase for higher sensitivity
+    void UpdateCameraRotation()
+    {
+        //Horizontal Rotation
+        transform.Rotate(Vector3.up, LookInputX());
+        
+        //Vertical Rotation
+        camVertAngle += LookInputY();
+        camVertAngle = Mathf.Clamp(camVertAngle, -89f, 89f);
+        playerCamera.transform.localEulerAngles = new Vector3(-camVertAngle, 0, 0);
+    }
+    void UpdateCursorState()
+    {
+        // Lock on Left Click
+        if (Input.GetMouseButtonDown(0)) Cursor.lockState = CursorLockMode.Locked;
+
+        // Unlock on Escape
+        if (Input.GetKeyDown(KeyCode.Escape)) Cursor.lockState = CursorLockMode.None;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCameraRotation();
+        UpdateCursorState();
+    }
+}
---
+++ Assets/Scenes/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed;
+    float MoveInputX()
+    {
+        return Input.GetAxis("Horizontal");
+    }
+    float MoveInputY()
+    {
+        return Input.GetAxis("Vertical");
+    }
+    // Start is called before the first frame update
+    Vector3 UpdatePlayerMovement()
+    {
+        Vector3 motion = transform.right * MoveInputX() + transform.forward * MoveInputY();
+        if (motion.magnitude > 1) motion.Normalize();
+        return motion*moveSpeed;
+    }
+
+    public CharacterController controller;//reference CharacterController
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Move(UpdatePlayerMovement()*Time.deltaTime);   
+    }
+}
